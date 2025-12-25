936f94f525cf6447699c3f8ee4a24d9a0ab85733
Salman Badr <mailsbadr@gmail.com>
2025-12-25 10:29:05 -0500

    SSHARE-14: added BrowserRouter, basic styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), /**
+    Fast Refresh (hot module replacement for React components)
+    JSX/TSX transformation
+    React-specific optimizations */
+    tailwindcss() //handles CSS processing and optimization without needing PostCSS configuration.
+  ],
 })
+
