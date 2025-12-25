b28f957d23645a4670427cc9b4d0ad4e3d531d3a
Salman Badr <mailsbadr@gmail.com>
2025-12-25 11:24:52 -0500

    SSHARE-15: added Home and AboutUs pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (<>
+        <main>
+            <h1>ScootShare Home</h1>
+        </main>
+    </>)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
     React-specific optimizations */
     tailwindcss() //handles CSS processing and optimization without needing PostCSS configuration.
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
 
