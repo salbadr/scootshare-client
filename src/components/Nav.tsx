5f08b10db8d161c8626684fa14458215ce75ac82
Salman Badr <mailsbadr@gmail.com>
2025-12-26 15:47:19 -0500

    SSHARE-16: added styling and updated build process to deploy

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import './Nav.css'
+
+type NavLinkClassNames = {
+    isActive: boolean;
+}
+
 export default function Nav() {
+
+    function generateClassNames({ isActive }: NavLinkClassNames) {
+        let className = "w-[100px] text-center hover:text-zinc-900";
+        if (isActive) {
+            className += ' active'
+        }
+
+        return className
+
+    }
+
     return (<nav className="flex justify-between border-t-1 border-b-1 border-gray-300 px-16 py-3 sticky top-0 shadow-md shadow-stone-200">
         <div>
             <span>Logo</span>
         </div>
 
         <div className="flex justify-evenly ">
-            <Link className="w-[100px] text-center" to={'/'}>Home</Link>
-            <Link className="w-[100px] text-center" to={'/about'}>About</Link>
+            <NavLink className={generateClassNames} to={'/'}>Home</NavLink >
+            <NavLink className={generateClassNames} to={'/about'}>About</NavLink >
         </div>
 
 
