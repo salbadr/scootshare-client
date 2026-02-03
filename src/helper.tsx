386be3acbde88c3cf6d0e7fcaa0e04781a884464
Salman Badr <mailsbadr@gmail.com>
2026-02-03 18:42:44 -0500

    34: updated to get images from CDN

--- src/helper.tsx
+++ src/helper.tsx
@@ -3,3 +3,7 @@ const CDN = import.meta.env.VITE_CDN
 export const siteImage=(filename: string)=>{
     return `${CDN}/site/${filename}`;
 }
+
+export const scooterImage=(filename?: string)=>{
+    return `${CDN}/scooters/${filename??'default.jpeg'}`;
+}
--- src/pages/Scooters.tsx
+++ src/pages/Scooters.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/components/Badge";
 import { Card } from "@/components/Card";
-import { siteImage } from "@/helper";
+import { scooterImage } from "@/helper";
 import { useScooters } from "@/hooks/useScooters";
 import type { ScooterType } from "@/types/scooter";
 
@@ -33,10 +33,10 @@ export default function Scooters() {
                 <h1 className="text-2xl md:text-4xl font-semi-bold">Available Scooters Near You</h1>
 
             </section>
-            <section className="grid grid-cols-3 gap-4 p-10">
+            <section className="grid md:grid-cols-3 gap-4 p-10">
                 {scooters.map(scooter => (
                     <Card key={scooter.id} className="hover:cursor-pointer p-0 bg-white" alignment="left">
-                        <img className="object-cover border-none rounded-t-xl" alt={scooter.name} title={scooter.name} src={siteImage('homepage_banner.jpg')} />
+                        <img className="aspect-square object-cover border-none rounded-t-xl" alt={scooter.name} title={scooter.name} src={scooterImage(scooter.image)} />
                         <div className="p-3 space-y-4">
                             <div className="flex justify-between">
                                 <h2 className="text-lg font-bold">{scooter.name}</h2>
