3f37f7ee886573edaea6bda7b0951aa7dcce7be6
Salman Badr <mailsbadr@gmail.com>
2026-02-03 21:58:36 -0500

    38: created the scooter details page

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,16 +1,31 @@
+import { ScooterTypes, type ScooterType } from "@/types/scooter"
 import type { ReactNode } from "react"
 import { twMerge } from "tailwind-merge"
 
 type BadgeProps = {
-    children: ReactNode
+    children: ReactNode,
+    type: ScooterType,
     className?: string
 }
 
-export function Badge({ children, className }: BadgeProps) {
+export function Badge({ type, children, className }: BadgeProps) {
 
+    const getBadgeColor = (type: ScooterType) => {
+
+        switch (type) {
+            case ScooterTypes.electric:
+                return 'bg-lime-600 text-white'
+            case ScooterTypes.gas:
+                return 'bg-amber-600 text-white'
+            case ScooterTypes.kick:
+                return 'bg-zinc-600 text-white'
+
+        }
+
+    }
     return (
         <div className=
-            {twMerge("border rounded-md p-1 w-24 text-center", className)}
+            {twMerge("border rounded-md p-1 w-24 text-center", getBadgeColor(type), className)}
         >
 
             {children}
--- src/pages/ScooterDetails.tsx
+++ src/pages/ScooterDetails.tsx
@@ -1,5 +1,44 @@
-export function ScooterDetails(){
+import { Badge } from "@/components/Badge";
+import { Button } from "@/components/Button";
+import { Card } from "@/components/Card";
+import { scooterImage } from "@/helper";
+import { useScooterDetails } from "@/hooks/useScooterDetails";
+import { useParams } from "react-router-dom"
+
+export function ScooterDetails() {
+    const { id } = useParams();
+
+    const { isLoading, data: scooter } = useScooterDetails(id);
+
+    if (isLoading) return <div>Loading...</div>
+
+    if (!scooter) {
+        return <div>No Scooter Details....</div>
+    }
+
     return (
-        <h1>Scooter Details</h1>
+
+        <section className="w-full h-full bg-amber-50">
+            <section className="p-10">
+                <h1 className="text-2xl md:text-4xl font-semi-bold">Scooter Details for {scooter.name}</h1>
+
+            </section>
+
+            <section className="flex justify-center pb-20">
+                <Card key={scooter.id} className="bg-white w-4xl" alignment="left">
+                    <img className="aspect-square object-cover border-none rounded-xl" alt={scooter.name} title={scooter.name} src={scooterImage(scooter.image)} />
+                    <div className="p-3 space-y-4">
+                        <h2 className="text-lg font-bold">Price: ${scooter.price}/hr</h2>
+                        <Badge type={scooter.type}>{scooter.type}</Badge>
+                        <p className="w-2xl">
+                            {scooter.description}
+                        </p>
+                        <Button className="bg-amber-300 w-full  border-amber-300 ">Rent Scooter</Button>
+                    </div>
+
+                </Card>
+            </section>
+
+        </section>
     )
 }
