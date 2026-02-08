4da7b1a8259e557a8228b64dc39b4e66a8ad2319
Salman Badr <mailsbadr@gmail.com>
2026-02-08 15:10:34 -0500

    40: used import.meta.env.DEV to check dev environment

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -1,9 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const environment = import.meta.env.MODE;
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = environment === 'development' ? import.meta.env.VITE_SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseKey = import.meta.env.DEV ? import.meta.env.VITE_SUPABASE_ANON_KEY : import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-        
