bebb66e4e2c3333bddb6cd3e72792fa7a00d4b43
Salman Badr <mailsbadr@gmail.com>
2026-02-08 15:08:08 -0500

    40: updated README and fixed env variables

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -1,9 +1,9 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-
+const environment = import.meta.env.MODE;
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = environment === 'development' ? import.meta.env.VITE_SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
