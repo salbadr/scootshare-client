59d7c305a9d418927c4e8e07d318fa97f0df9cd1
Salman Badr <mailsbadr@gmail.com>
2026-02-09 14:27:29 -0500

    43: created a layout route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from '@/components/Layout'
 
-import Nav from '@/components/Nav'
-import Footer from '@/components/Footer'
 import Home from '@/pages/Home'
 import About from '@/pages/AboutUs'
 import Scooters from '@/pages/Scooters'
@@ -24,18 +23,17 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Nav />
-        <main>
-          <Routes>
+        <Routes>
+          <Route element={<Layout />}>
             <Route path='/' element={<Home />} />
             <Route path='/home' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/scooters' element={<Scooters />} />
             <Route path='/scooters/:id' element={<ScooterDetails />} />
 
-          </Routes>
-        </main>
-        <Footer />
+          </Route>
+
+        </Routes>
 
       </BrowserRouter>
     </QueryClientProvider>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,29 @@ import { Link } from "react-router-dom";
 
 export default function Footer (){
     return ( <footer className='bg-zinc-800 p-20 text-zinc-400 '>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-zinc-400 pb-10 mb-10'>
-          <div className='flex flex-col gap-4'>
+        <section className='grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-zinc-400 pb-10 mb-10'>
+          <section className='flex flex-col gap-4'>
             <h2 className="text-2xl text-white font-semibold">ScootShare</h2>
             <p className="w-3xs">Making cities more accessible, one ride at a time</p>
-          </div>
+          </section>
 
-          <div className='flex flex-col gap-3'>
+          <section className='flex flex-col gap-3'>
             <h3 className="text-lg text-white font-semibold">Company</h3>
             <Link to="/about" className='hover:text-white'>About Us</Link>
             <Link to="/contact" className='hover:text-white'>Contact</Link>
-          </div>
-          <div className='flex flex-col gap-3'>
+          </section>
+          <section className='flex flex-col gap-3'>
             <h3 className="text-lg text-white font-semibold">Scooters</h3>
             <Link to="/about" className='hover:text-white'>Browse Scooters</Link>
             <Link to="/contact" className='hover:text-white'>How it Works</Link>
-          </div>
-          <div className='flex flex-col gap-3'>
+          </section>
+          <section className='flex flex-col gap-3'>
             <h3 className="text-lg text-white font-semibold">Support</h3>
             <Link to="/about" className='hover:text-white'>FAQ</Link>
             <Link to="/contact" className='hover:text-white'>Safety</Link>
-          </div>
+          </section>
 
-        </div>
+        </section>
         <span className='text-sm'>&copy; 2025 ScootShare. All rights reserved.</span>
 
       </footer>)
